170828350e34ed2ec24fcc5365b756d2a4bb4a4d
v15hv4 <vishva2912@gmail.com>
2020-12-31 02:49:08 +0530

    Parse player status and update toggle button

--- spotify.sh
+++ spotify.sh
@@ -2,6 +2,11 @@
 
 # read dbus-monitor 
 dbus-monitor --profile "interface=org.freedesktop.DBus.Properties,member=PropertiesChanged" | while read -r line; do
+    # parse status
+    STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | tail -n 1 | sed 's/.*"\(.*\)".*/\1/g') 
+
+    # change toggle icon
+    polybar-msg hook spotify-control $([ "$STATUS" = "Playing" ] && echo 1 || echo 2) | 2&>1 /dev/null
 
     # parse metadata
     METADATA=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata') 
@@ -9,7 +14,7 @@ dbus-monitor --profile "interface=org.freedesktop.DBus.Properties,member=Propert
     TITLE=$(echo "$METADATA" | grep -A1 "xesam:title" | tail -n 1 | sed 's/.*"\(.*\)".*/\1/g')
 
     # sneaky adblock
-    if [ "$TITLE" == "Advertisement" ] || [ "$TITLE" == "Spotify" ]; then
+    if [ "$TITLE" = "Advertisement" ] || [ "$TITLE" = "Spotify" ]; then
         pkill spotify
         spotify 2&>1 /dev/null &
         sleep 4
