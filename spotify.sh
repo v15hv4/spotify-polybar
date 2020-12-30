5f7405eca9c48119fcbe541fde34882b1f3e9e31
v15hv4 <vishva2912@gmail.com>
2020-12-31 02:19:02 +0530

    Fetch artist and improve formatting

--- spotify.sh
+++ spotify.sh
@@ -17,13 +17,16 @@
 # done
 
 dbus-monitor --profile "interface=org.freedesktop.DBus.Properties,member=PropertiesChanged" | while read -r line; do
-    CURRENT="$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | sed -n '/title/{n;p;}' | cut -d'"' -f2)"
-    if [ "$CURRENT" == "Advertisement" ] || [ "$CURRENT" == "Spotify" ]; then
+    METADATA=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata') 
+    ARTIST=$(echo "$METADATA" | grep -A2 "xesam:artist" | tail -n 1 | sed 's/.*"\(.*\)".*/\1/g')
+    TITLE=$(echo "$METADATA" | grep -A1 "xesam:title" | tail -n 1 | sed 's/.*"\(.*\)".*/\1/g')
+
+    if [ "$TITLE" == "Advertisement" ] || [ "$TITLE" == "Spotify" ]; then
         pkill spotify
         spotify 2&>1 /dev/null &
         sleep 4
         dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
     else
-        echo "$CURRENT"
+        echo "$ARTIST - $TITLE"
     fi
 done
